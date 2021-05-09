a4110d84623bc156d3ffde87a5f933e5d168f056
Victor França <victor@fr4nca.io>
2021-05-09 18:30:03 -0300

    fixed

--- fabfile.py
+++ fabfile.py
@@ -67,7 +67,7 @@ def shared(pool, c):
 
 def project_requirements(pool, c):
     # update python package
-    command = f"source {c.config.virtual}; cd {c.config.current_release}; yarn; yarn build; yarn sequelize db:migrate"
+    command = f"source {c.config.virtual}; cd {c.config.current_release}; yarn; yarn build; yarn sequelize db:migrate; cd dist; mkdir uploads;"
 
     pool.run(command, shell="/bin/bash")
 
