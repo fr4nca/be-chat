c2accc6ba109e071b7470ec099a98445154191da
Victor França <victor@fr4nca.io>
2021-05-10 09:41:28 -0300

    fixed

--- src/io.ts
+++ src/io.ts
@@ -38,6 +38,7 @@ export default (server: http.Server): Namespace[] => {
     });
 
     // eslint-disable-next-line consistent-return
+
     io.use((socket, next) => {
         if (!socket.handshake.auth.token) {
             return next(new Error("Unauthorized"));
