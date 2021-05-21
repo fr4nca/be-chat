fd9c8762fc1952d1fec7d6b7380c177430d4390c
Victor França <victor@fr4nca.io>
2021-05-20 22:15:00 -0300

    fixed

--- src/multer.ts
+++ src/multer.ts
@@ -1,16 +1,9 @@
 import multer from "multer";
 import path from "path";
 
-import { debug } from "./config/config.json";
-
 const storage = multer.diskStorage({
   destination(_, __, cb) {
-    cb(
-      null,
-      debug
-        ? path.join(__dirname, "uploads")
-        : "/srv/deadpool.cloudez.io/etc/uploads",
-    );
+    cb(null, path.join(__dirname, "uploads"));
   },
   filename(_, file, cb) {
     cb(null, `${+new Date()}-${file.originalname}`);
